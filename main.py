ce286fbb07bb714a68b161565f7988a223f505da
Norbert <nobo155@gmail.com>
2021-02-24 03:00:40 +0100

    cars and motors
    fuck yeah

--- main.py
+++ main.py
@@ -74,18 +74,26 @@ def set_location(txt):
     except:
         print('set location failed')
 
+def set_max_price(txt):
+    global act_max_price
+    global act_url
+    try:
+        act_max_price = txt
+    except:
+        print('set max price failed')
+
+
+
 
 def choose_cat():
     print('Wybierz kategorię spośród podanych, wpisz liczbę:')
-    global act_cat
     for indx, c in enumerate(cat):
         print(str(indx)+'. '+c)
-    act = input('Interesuje Cię kategoria numer?  ')
+    act = int(input('Interesuje Cię kategoria numer?  '))
     try:
-        if int(act) == 3 or int(act) == 0:
-            raise
-        print('\nWybrana kategoria to: ' + cat[int(act)] +'\n')
-        act_cat = act
+        if act == 3 or act == 0:
+            print(f'Wybrana Kategoria: {cat[act]}')
+            return act
     except:
         print('Kategoria nie jest obsługiwana')
         choose_cat()
@@ -127,20 +135,6 @@ def choose_brand():
     return act
 
 
-'''
-Task manager attempt
-
-def runMenu(x):
-        print('wtf')
-        switcher = {
-            0: (chooseCat()),
-            1: (chooseBrand()),
-            2: (chooseModel()),
-        }
-        return switcher.get(x, 'nothing')
-'''
-
-
 def choose_model():
     r = session.get(act_url)
     r.html.render()
@@ -162,6 +156,7 @@ def choose_model():
     act = input('Wybierz model:   ')
     return act
 
+
 def get_links(urll):
     # r = session.get(act_url)
     # r.html.render()
@@ -178,12 +173,11 @@ def get_links(urll):
 
     return adds
 
+
 def get_details(url):
     details = []
     params  = []
     r = requests.get(url=str(url))
-
-
     soup = BeautifulSoup(r.text, 'html.parser')
 
     labels = soup.find_all('span', {'class': 'offer-params__label'})
@@ -210,9 +204,28 @@ def get_details(url):
 
 
 def f_link_builder():
+    OMParams = [cat[act_cat], act_brand, act_model, act_from, act_location, act_min_price, act_max_price,
+                act_to, act_mileage_from, act_mileage_to, act_fuel_type, act_country]
     link = f'https://www.otomoto.pl'
+    query = False
     for p in OMParams:
         if p:
+            if p == act_min_price:
+                if not query:
+                    link += '/'+f'?search%5Bfilter_float_price%3Afrom%5D={str(p)}'
+                    query = True
+                    continue
+                else:
+                    link += '/'+f'&search%5Bfilter_float_price%3Afrom%5D={str(p)}'
+
+            if p == act_max_price:
+                if not query:
+                    link += '/'+f'?search%5Bfilter_float_price%3Ato%5D={str(p)}'
+                    query = True
+                    continue
+                else:
+                    link += '/'+f'&search%5Bfilter_float_price%3Ato%5D={str(p)}'
+
             if p == act_from:
                 link += '/od-'+str(p)
             else:
@@ -228,31 +241,27 @@ if __name__ == '__main__':
 
     # act_cat = chooseCat()
     # act_url = f'{act_url}/{cat[3]}'
-    choose_cat()
+    act_cat = choose_cat()
     # act_brand = choose_brand()
     # act_url = f'{act_url}/{act_brand}'
-    set_brand('BMW')
-    # act_model = chooseModel()
+    set_brand('Porsche')
+    # act_model = choose_model()
     # act_url = f'{act_url}/{act_model}'
-    set_model('C1')
-
-    # set_from(2021)
-
-    #set_location('/Warszawa')
+    set_model('Panamera')
+    set_from(2016)
+    # set_location('Warszawa')
+    #set_max_price(55000)
 
     #act_url = 'https://www.otomoto.pl/motocykle-i-quady/suzuki/rm-z/'
 
     # print(act_url)
 
-    OMParams = [cat[act_cat], act_brand, act_model, act_from, act_location, act_min_price, act_max_price,
-                act_to, act_mileage_from, act_mileage_to, act_fuel_type, act_country]
-
     f_link = f_link_builder()
     print(f_link)
-    print(cat[0])
+    # print(cat[0])
     #
     # f_link = f'https://www.otomoto.pl/{act_cat}/{act_brand}/{act_model}/od-{act_from}{act_location}/?search%5Bfilter_float_price%3Afrom%5D={act_min_price}&search%5Bfilter_float_price%3Ato%5D={act_max_price}&search%5Bfilter_float_year%3Ato%5D={act_to}&search%5Bfilter_float_mileage%3Afrom%5D={act_mileage_from}&search%5Bfilter_float_mileage%3Ato%5D={act_mileage_to}&search%5Bfilter_enum_fuel_type%5D%5B0%5D={act_fuel_type}&search%5Bfilter_enum_country_origin%5D%5B0%5D={act_country}&'
     #
-    # links = get_links(f_link)
-    # for link in links:
-    #     print(*get_details(link), sep='\n')
+    links = get_links(f_link)
+    for link in links:
+        print(*get_details(link), sep='\n')
